46e655894ef78601faebdee9a2158e53ba5df0c4
Gakeniii <105628080+Gakeniii@users.noreply.github.com>
2022-05-17 23:03:43 +0300

    lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+
+#python basics
+
+#this is a comment
+
+#adding a number  
+
+#This for a 
+#Multi / many lines
+#Comment
+#
+print("my name is Sifa Gakeni")
+print("i am 18years old")
+print("i am from nairobi")
+print("i like reading novels")
+print("i like the STEM training programme")
+print("i like watching tiktok")
