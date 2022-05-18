3c41cfc989afbc3bb3cb672398b2e66afc4ab3d5
Gakeniii <105628080+Gakeniii@users.noreply.github.com>
2022-05-18 10:05:04 +0300

    assignment1.py

---
+++ lesson2.py
@@ -0,0 +1,26 @@
+#my name is an interger
+
+from datetime import date
+
+
+age = 18
+
+noOfSiblings = 3
+
+dateOfBirth = 23
+yearOfBirth = 2003
+
+#strings
+myFirstName = "Sifa"
+mySecondName = "Gakeni"
+
+favouriteFood = "beef"
+schoolAttended = "St.Catherine girls"
+
+
+print("I am" + myFirstName)
+print("my favourite food" + favouriteFood)
+print("I attended" + schoolAttended)
+print("I am" + str(age) + "years old")
+print("I have" + str(noOfSiblings) + "siblings")
+print("I was born in" + str(yearOfBirth))
