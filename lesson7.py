0025e39b874fcd6f144dcfd5dabcd110641d8441
Gakeniii <105628080+Gakeniii@users.noreply.github.com>
2022-05-19 11:16:26 +0300

    lesson on list and loops

---
+++ lesson7.py
@@ -0,0 +1,10 @@
+#LOOPS
+school = ('Joy' , 'Hope' , 'Mercy' , 'Happy')
+pupil = ('Mark' , 'Peace' , 'Patience' , 'Amani')
+print(school[0] , pupil[0]) 
+#Hard way
+#print("I am {pupil[0]} and I school at {school[0]}")
+#Easy way
+sentence = f"Hello my name is {pupil}"
+for pupil in pupil:
+ print(F'Hello i am pupil {pupil}')
