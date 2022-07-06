202ac205fcb7a33d69a221552c6e5f3c9dbb479a
Gakeniii <105628080+Gakeniii@users.noreply.github.com>
2022-07-06 10:56:45 +0300

    Add files via upload

---
+++ WEB/JS/index.js
@@ -0,0 +1,17 @@
+let message = "Hello world"
+console.log(message)
+document.write(message)
+// Single line comment in Javascript
+/* 
+Multi-line comment in Javascript
+*/ 
+
+/*let num_1= 134
+let num_2= 134
+let sum= num_1 + num_2
+console.log(sum)
+*/
+
+function submitButton(){
+    alert("Button clicked")
+}
